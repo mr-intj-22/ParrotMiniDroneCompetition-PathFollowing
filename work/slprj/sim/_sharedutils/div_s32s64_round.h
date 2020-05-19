6d4e3f59cdfc4c4f201a870907b2cfe483d3d533
Abdulmalek SHEFAT <absulmalek.s.a.shefat@gmail.com>
2020-05-19 12:47:35 +0200

    First commit

---
+++ work/slprj/sim/_sharedutils/div_s32s64_round.h
@@ -0,0 +1,8 @@
+#ifndef SHARE_div_s32s64_round
+#define SHARE_div_s32s64_round
+#include "rtwtypes.h"
+#include "multiword_types.h"
+
+extern int32_T div_s32s64_round(int64_T numerator, int64_T denominator);
+
+#endif
